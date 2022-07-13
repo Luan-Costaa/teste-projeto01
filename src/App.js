9d47eb1c154297fa27a47dc8169ce49e14b43de6
Luan Costa <luan.medeiroscosta.pires@gmail.com>
2022-07-13 16:48:28 -0300

    teste02

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-const[titulo,setTitulo] = useState("Luan");
+const[titulo,setTitulo] = useState("Parça");
   return (
     <div className='container'>
       <div>
         <h1>{titulo}</h1>
-        <button onClick={function(){setTitulo("Monique")}}>Alterar</button>
+        <button onClick={function(){setTitulo("O bagui ta on kkk")}}>Alterar</button>
       </div>
     </div>
   );
